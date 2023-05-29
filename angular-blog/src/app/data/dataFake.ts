841ca95b76e56c6aea87b87cfc8557e511e11186
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-05-29 03:18:09 -0300

    adicionado dataFake (simulando BD)

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": "1",
+        "title": "NOVIDADES NO MULTIVERSO DA MARVEL",
+        "description": "Marvel studios anuncia novos heróis.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure laudantium corporis illo quibusdam nostrum atque aperiam quos, sunt officia aliquid.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAwlQ9LlZJL0huWSH00FnF-9XkVDLhAgkIlg&usqp=CAU"
     },
     {
-        "id": 2,
-        "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": "2",
+        "title": "Serie Loki ganharam nova temporada em 2024",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "photoCover":"https://sagresonline.com.br/wp-content/uploads/2021/05/loki-poster-serie-da-marvel-disney-com-tom-hiddleston.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Iron Men pode ressurgir na saga Marvel",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "photoCover":"https://cinepoca.com.br/wp-content/uploads/2021/10/Homem-de-Ferro-capa.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Luffy Poderá aparecer em próximos filmes da MCU",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "photoCover":"https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/05/luffy-one-piece-1024x576.webp?resize=1024%2C576&ssl=1"
     }
+    
 ]
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -6,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = "https://www.planocritico.com/wp-content/uploads/2017/07/Homem-Aranha-Filmes-Ranking.jpg"
-  contentTitle:string = "MINHA NOTICIA"
-  contentDescription = "Ola mundo, blablabla bla bla"
-  constructor() { }
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription = ""
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id === id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+    
   }
 
 }
